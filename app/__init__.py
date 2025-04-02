f0a60dcb0f8a6f534213586e1bbd830e904c8ef1
Fernando Pontes <fernandopontes@outlook.com>
2025-04-02 11:31:08 -0300

    Refactor code to check DATABASE_URL is set correctly

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,12 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
+    database_url = os.getenv('DATABASE_URL')
+    if not database_url:
+        raise RuntimeError("A variável de ambiente 'DATABASE_URL' não está definida!")
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
